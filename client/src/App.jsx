945fca2ddda930e1884d63b5a337c1ae46d20a10
Basit <basitmanzoor438@gmail.com>
2025-02-02 00:32:41 +0530

    added a user regster page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,8 @@ import { RouterProvider } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Layout from './pages/layout'
+import Login from './pages/login/Login'
+import Register from './pages/register/Register'
 
 import './App.css'
 
@@ -21,6 +23,14 @@ function App() {
         {
           path : "/about",
           element : <About/>
+        },
+        {
+          path : "/login",
+          element : <Login/>
+        },
+        {
+          path : "/register",
+          element : <Register/>
         }
       ]
     }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { useState , useEffect } from 'react'
 
 function Home() {
   return (
-    <div>
-      this is home page
-    </div>
+   <>
+
+   </>
   )
 }
 
