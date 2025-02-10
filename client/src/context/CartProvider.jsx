ba6559dd6a7f27f8d554e9feeecab0de01739281
Basit <basitmanzoor438@gmail.com>
2025-02-10 20:28:56 +0530

    REMOVED console logs and improved error handling in CartProvider context

--- client/src/context/CartProvider.jsx
+++ client/src/context/CartProvider.jsx
@@ -26,7 +26,6 @@ export function CartProvider({ children }) {
           { productId, quantity },
           { withCredentials: true }
         );
-        console.log("Product added to cart:", response.data);
         setCart(response.data.cart);
         toast.success("Product added to cart!");
       } catch (error) {
@@ -36,7 +35,7 @@ export function CartProvider({ children }) {
           const refer = encodeURIComponent(location.pathname + location.search);
           navigate(`/login?refer=${refer}`);
         } else {
-          console.error("Error adding product to cart:", error);
+          
           toast.error("Failed to add product to cart. Please try again later.");
         }
       }
@@ -46,9 +45,9 @@ export function CartProvider({ children }) {
       try {
         const response = await instance.get("cart", { withCredentials: true });
         setCart(response.data.cart);
-        console.log("Cart fetched:", response.data.cart);
+       
       } catch (error) {
-        console.error("Error fetching cart:", error);
+        
         toast.error("Failed to fetch cart.");
       }
     }
@@ -60,10 +59,10 @@ export function CartProvider({ children }) {
           { productId, quantity },
           { withCredentials: true }
         );
-        console.log("Cart updated:", response.data);
+      
         setCart(response.data.cart);
       } catch (error) {
-        console.error("Error updating cart:", error);
+      
         toast.error("Failed to update cart. Please try again later.");
       }
     }
@@ -73,7 +72,7 @@ export function CartProvider({ children }) {
         const response = await instance.delete("cart/remove/" + productId, {
           withCredentials: true,
         });
-        console.log("Product removed from cart:", response.data);
+       
         // Update the cart state with the new cart data:
         setCart(response.data.cart);
         toast.success("Product removed from cart!");
